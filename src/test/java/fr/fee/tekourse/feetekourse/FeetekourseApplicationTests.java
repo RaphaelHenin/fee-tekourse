8b4b88dcc9988a92237552f5f13c4bd0410ac289
RaphaelHenin <raphael.henin@laposte.net>
2023-11-25 22:25:23 +0100

    Generation du back Spring Boot

---
+++ src/test/java/fr/fee/tekourse/feetekourse/FeetekourseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fee.tekourse.feetekourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeetekourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
