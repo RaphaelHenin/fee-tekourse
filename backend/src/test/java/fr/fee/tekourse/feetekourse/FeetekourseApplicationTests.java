3ad0e1571260a84d832f86de0b0dc67c07ac0844
RaphaelHenin <raphael.henin@laposte.net>
2024-01-10 00:18:44 +0100

    Ajout d'un assert pour le TI du Application.java

--- backend/src/test/java/fr/fee/tekourse/feetekourse/FeetekourseApplicationTests.java
+++ backend/src/test/java/fr/fee/tekourse/feetekourse/FeetekourseApplicationTests.java
@@ -1,13 +1,20 @@
 package fr.fee.tekourse.feetekourse;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FeetekourseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals("feetekourse", this.applicationName);
+    }
 
 }
