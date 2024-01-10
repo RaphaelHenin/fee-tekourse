25b43ae687dd1c49cd9c84218053b2f64b83ff59
RaphaelHenin <raphael.henin@laposte.net>
2024-01-10 22:55:14 +0100

    [USECASE1] Mise en place de liquibase et du modèle de donnée

--- backend/src/main/java/fr/fee/tekourse/feetekourse/configuration/LocalSecurityConfig.java
+++ backend/src/main/java/fr/fee/tekourse/feetekourse/configuration/LocalSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class LocalSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable).build();
+        return http
+                //Pour effectuer des curl en local sur les APIs
+                .csrf(AbstractHttpConfigurer::disable)
+                //Pour pouvoir utiliser la console h2
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
     }
 }
